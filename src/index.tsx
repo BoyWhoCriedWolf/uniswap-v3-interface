68f0027ca09f7ada0797280ff75eae02e465a304
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-07-16 01:16:23 +0800

    feat: initial mui package

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import UniswapV3Widget from "./uniswap-v3-widget";
+
+export default UniswapV3Widget;
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UniswapV3Widget />
   </React.StrictMode>
 );
 
---
+++ src/uniswap-v3-widget/index.tsx
@@ -0,0 +1,3 @@
+import UniswapV3Widget from "./UniswapV3Widget";
+
+export default UniswapV3Widget;
